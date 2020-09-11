c411be5ef3a0eaea74f6f00958f317da7c59fdd5
berryy <gabe@kjsl.org>
2020-09-11 18:10:07 -0400

    gelbooru.py now parses XML instead of JSON

--- snakebooru/gelbooru.py
+++ snakebooru/gelbooru.py
@@ -14,7 +14,7 @@ class Gelbooru:
         self.api_key = api_key
         self.user_id = user_id
         self.page_num = randint(0, 19)
-        self.booru_url = 'https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1'
+        self.booru_url = 'https://gelbooru.com/index.php?page=dapi&s=post&q=index'
         self.comment_url = 'https://gelbooru.com/index.php?page=dapi&s=comment&q=index'
 
     def __tagifier(self, unformated_tags):
@@ -23,79 +23,108 @@ class Gelbooru:
     
     # Get a bunch of posts based on a limit and tags that the user enters.
     def get_posts(self, tags='', limit=100):
+        posts = []
         tags = self.__tagifier(tags)
         final_url = self.booru_url + f'&limit={str(limit)}&tags={tags}&pid={self.page_num}&api_key={self.api_key}&user_id={self.user_id}'
+        
         try:
             urlobj = urlreq.urlopen(final_url)
-            json_response = json.load(urlobj)
+            data = ET.parse(urlobj)
             urlobj.close()
-        except json.decoder.JSONDecodeError:
+            root = data.getroot()
+        except ET.ParseError:
             return None
 
+        for post in root:
+            posts.append(post.attrib)
+        
         temp = 4
-        # Reduces search if json_response is an empty list
-        while len(json_response) == 0: 
-            self.page_num = randint(0, temp) 
-            # Further reduction if random integer fails again
+        attempts = 5
+        while len(posts) == 0:
+            self.page_num = randint(0, temp)
             if temp > 0:
-                temp += -1
+                temp -= 1
             else:
                 pass
-            final_url = self.booru_url + f'&limit={str(limit)}&tags={tags}&pid={self.page_num}&api_key={self.api_key}&user_id={self.user_id}'
+            final_url = self.booru_url + f'&limit={str(limit)}&tags={tags}&pid={self.page_num}&api_key={self.api_key}&user_id={self.user_id}' 
             try:
                 urlobj = urlreq.urlopen(final_url)
-                json_response = json.load(urlobj)
+                data = ET.parse(urlobj)
                 urlobj.close()
-            except json.decoder.JSONDecodeError:
+                root = data.getroot()
+            except ET.ParseError:
                 return None
 
-        images = self.__link_images(json_response)
+            attempts -= 1
+            if attempts == 0:
+                return None
+            else:
+                continue
+            for post in root:
+                posts.append(post.attrib)
+        
+        images = self.__link_images(posts)
         return images
 
     # Get a single image based on tags that the user enters.
     def get_single_post(self, tags=''):
         tags = self.__tagifier(tags)
-        post_url = self.booru_url + f'&limit=1&tags={tags}&pid={self.page_num}&api_key={self.api_key}&user_id={self.user_id}'
+        posts = []
+        final_url = self.booru_url + f'&limit=1&tags={tags}&pid={self.page_num}&api_key={self.api_key}&user_id={self.user_id}'
         try:
-            urlobj = urlreq.urlopen(post_url)
-            json_response = json.load(urlobj)
+            urlobj = urlreq.urlopen(final_url)
+            data = ET.parse(urlobj)
             urlobj.close()
-        except json.decoder.JSONDecodeError:
+            root = data.getroot()
+        except ET.ParseError:
             return None
-
+        
+        for post in root:
+            posts.append(post.attrib)
+        
+        # reduces search if no results found
         temp = 4
-        # Reduces search if json_response is an empty list
-        while len(json_response) == 0:
+        attempts = 5
+        while len(posts) == 0:
             self.page_num = randint(0, temp)
-            # Further reduction if random integer fails again
             if temp > 0:
-                temp += -1
+                temp -= 1
             else:
                 pass
-            post_url = self.booru_url + f'&limit=1&tags={tags}&pid={self.page_num}&api_key={self.api_key}&user_id={self.user_id}'
+            final_url = self.booru_url + f'&limit=1&tags={tags}&pid={self.page_num}&api_key={self.api_key}&user_id={self.user_id}' 
             try:
-                urlobj = urlreq.urlopen(post_url)
-                json_response = json.load(urlobj)
+                urlobj = urlreq.urlopen(final_url)
+                data = ET.parse(urlobj)
                 urlobj.close()
-            except json.decoder.JSONDecodeError:
+                root = data.getroot()
+            except ET.ParseError:
                 return None
+
+            attempts -= 1
+            if attempts == 0:
+                return None
+            else:
+                continue
         
-        image = self.__link_images(json_response)
+        image = self.__link_images(posts)
         return image
     
     def get_random_post(self):
         self.page_num = randint(0, 200)
-        post_url = self.booru_url + f'&pid={self.page_num}&api_key={self.api_key}&user_id={self.user_id}'
+        posts = []
+        final_url = self.booru_url + f'&pid={self.page_num}&api_key={self.api_key}&user_id={self.user_id}'
+        
         try:
-            urlobj = urlreq.urlopen(post_url) 
-            json_response = json.load(urlobj)
+            urlobj = urlreq.urlopen(final_url)
+            data = ET.parse(urlobj)
             urlobj.close()
-        except json.decoder.JSONDecodeError:
+            root = data.getroot()
+        except ET.ParseError:
             return None
         
-        temp = [json_response[randint(0,99)]]
-        image = self.__link_images(temp)
-        return image[0]
+        posts.append(root[randint(0,99)].attrib)
+        image = self.__link_images(posts)
+        return image
         
     # Get comments from a post using post_id
     def get_comments(self, post_id):
@@ -105,7 +134,7 @@ class Gelbooru:
             urlobj = urlreq.urlopen(final_url)
             data = ET.parse(urlobj)
             urlobj.close()
-        except json.decoder.JSONDecodeError:
+        except:
             return None
 
         root = data.getroot()
@@ -121,7 +150,7 @@ class Gelbooru:
         if len(comment_list) == 0:
             return "No comments found"
         else:
-            return comment_list # Returns list of dictionaries
+            return comment_list
     
     # Get data for a post
     def get_post_data(self, post_id):
@@ -130,12 +159,12 @@ class Gelbooru:
             urlobj = urlreq.urlopen(data_url)
             data = ET.parse(urlobj)
             urlobj.close()
-        except json.decoder.JSONDecodeError:
+        except:
             return None
 
         root = data.getroot()
 
-        return root[0].attrib # Returns a dictionary
+        return root[0].attrib
 
     # Private function to create a post URL and a related image URL
     def __link_images(self, response):
